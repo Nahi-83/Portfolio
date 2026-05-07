71bab7f4e98d93524e0d65ebb0f1a4e4270508e5
Nahiara Millahuala <nahi042007@gmail.com>
2026-05-07 00:19:31 -0300

    Aun no está terminado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Navbar} from './componentes/navbar/navbar';
+import { Inicio} from './componentes/inicio/inicio';
+import { About} from './componentes/about/about';
+import { Projects } from './componentes/projects/projects';
+import { Contacts } from './componentes/contacts/contacts';
+import { Header} from './componentes/header/header';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'navbar', component: Navbar},
+    {path: 'inicio', component: Inicio},
+    {path: 'about', component: About},
+    {path: 'project', component: Projects},
+    {path: 'contacts', component: Contacts},
+    {path: 'header', component: Header},
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { About } from "./componentes/about/about";
+import { Contacts } from "./componentes/contacts/contacts";
+import { Inicio } from "./componentes/inicio/inicio";
+import { Navbar } from "./componentes/navbar/navbar";
+import { Projects } from "./componentes/projects/projects";
+import { Header } from './componentes/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, About, Contacts, Inicio, Navbar, Projects, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
