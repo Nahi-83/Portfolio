983e59dc4244eb58bd98585dfacb9840436d33a1
Nahiara Millahuala <nahi042007@gmail.com>
2026-06-02 17:55:53 -0300

    Corregí los enlaces del navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,7 @@ import { Contacts } from './pages/contacts/contacts';
 
 export const routes: Routes = [
     {path: 'navbar', component: Navbar},
-    {path: '', component: Inicio},
+    {path: 'inicio', component: Inicio},
     {path: 'about', component: About},
     {path: 'projects', component: Projects},
     {path: 'contacts', component: Contacts},
